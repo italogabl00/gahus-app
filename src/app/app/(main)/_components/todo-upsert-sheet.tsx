fdb92ede9132534798b954791d1a15d787d32668
italobgabl00 <italogabriel8856@gmail.com>
2024-09-30 14:58:22 -0300

    Feat: return to project

--- src/app/app/(main)/_components/todo-upsert-sheet.tsx
+++ src/app/app/(main)/_components/todo-upsert-sheet.tsx
@@ -1,6 +1,6 @@
-'use client'
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
+"use client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import {
   Sheet,
   SheetContent,
@@ -9,75 +9,80 @@ import {
   SheetHeader,
   SheetTitle,
   SheetTrigger,
-} from "@/components/ui/sheet"
-import { useRef } from "react"
-import { Todo } from "../type"
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
-import { useForm } from "react-hook-form"
-import { upsertTodo } from "../actions"
+} from "@/components/ui/sheet";
+import { useRef } from "react";
+import { Todo } from "../type";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import { upsertTodo } from "../actions";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { upsertTodoSchema } from "../schema";
+import { useRouter } from "next/router";
 
 type TodoUpsertSheetProps = {
-    children?: React.ReactNode
-    defaultValue?: Todo
-}
+  children?: React.ReactNode;
+  defaultValue?: Todo;
+};
 
 export function TodoUpsertSheet({ children }: TodoUpsertSheetProps) {
-
-    const ref = useRef<HTMLDivElement>(null)
-
+  const ref = useRef<HTMLDivElement>(null);
+  const router = useRouter();
   const form = useForm({
-    resolver: 
-  })
+    resolver: zodResolver(upsertTodoSchema),
+  });
 
-  const onSubmit = form.handleSubmit((data) => {
-    await upsertTodo(data)
-
-    console.log(data)
-  })
-
+  const onSubmit = form.handleSubmit(async (data) => {
+    await upsertTodo(data);
+    router.refresh();
+  });
 
   return (
     <Sheet>
       <SheetTrigger asChild>
-        <div ref={ref}>
-            {children}
-        </div>
+        <div ref={ref}>{children}</div>
       </SheetTrigger>
-    
+
       <SheetContent>
-          <Form {...form}>
-      <form onSubmit={onSubmit} className="space-y-8h-screen ">
-        <SheetHeader>
-          <SheetTitle>Create todo</SheetTitle>
-          <SheetDescription>
-            Make changes to your profile here. Click save when you're done.
-          </SheetDescription>
-        </SheetHeader>
-       
-        <FormField
-          control={form.control}
-          name="title"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Title</FormLabel>
-              <FormControl>
-                <Input placeholder="Enter your todo title" {...field} />
-              </FormControl>
-              <FormDescription>
-                This will be the publicy displayed name for the task
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-       
-        <SheetFooter className="mt-auto">
-            <Button type="submit">Save changes</Button>
-        </SheetFooter>
-        </form>
+        <Form {...form}>
+          <form onSubmit={onSubmit} className="space-y-8h-screen ">
+            <SheetHeader>
+              <SheetTitle>Create todo</SheetTitle>
+              <SheetDescription>
+                Make changes to your profile here. Click save when you're done.
+              </SheetDescription>
+            </SheetHeader>
+
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Title</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter your todo title" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    This will be the publicy displayed name for the task
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <SheetFooter className="mt-auto">
+              <Button type="submit">Save changes</Button>
+            </SheetFooter>
+          </form>
         </Form>
       </SheetContent>
-  
     </Sheet>
-  )
+  );
 }
--- src/app/app/(main)/actions.ts
+++ src/app/app/(main)/actions.ts
@@ -1,84 +1,78 @@
-'use server'
+"use server";
 
-import { auth } from "@/services/auth"
-import { prisma } from "@/services/database"
-import { upsertTodoSchema } from "./schema"
-import { z } from "zod"
+import { auth } from "@/services/auth";
+import { prisma } from "@/services/database";
+import { upsertTodoSchema } from "./schema";
+import { z } from "zod";
 
 export async function getUserTodos() {
-    const session = await auth() 
-        
-const todos = await prisma.todo.findMany({
-where: {
-    userId: session?.user?.id,
+  const session = await auth();
 
-},
-})
+  const todos = await prisma.todo.findMany({
+    where: {
+      userId: session?.user?.id,
+    },
+  });
 
-    
-    return todos
+  return todos;
 }
 
 export async function upsertTodo(input: z.infer<typeof upsertTodoSchema>) {
-    const session = await auth() 
+  const session = await auth();
 
-    if(!session?.user?.id) {
-        return {
-            error: 'Not Authorized',
-            data: null,
-        }
+  if (!session?.user?.id) {
+    return {
+      error: "Not Authorized",
+      data: null,
+    };
+  }
+
+  if (input.id) {
+    const todo = await prisma.todo.findUnique({
+      where: {
+        userId: session?.user?.id,
+      },
+      select: {
+        id: true,
+      },
+    });
+
+    if (!todo) {
+      return {
+        error: "Not Found",
+        data: null,
+      };
     }
 
-    
+    const updatedTodo = await prisma.todo.update({
+      where: {
+        id: input.id,
+        userId: session?.user?.id,
+      },
+      data: {
+        title: input.title,
+        doneAt: input.doneAt,
+      },
+    });
+    return {
+      error: null,
+      data: updatedTodo,
+    };
+  }
 
-    if(input.id) {
-        const todo = await prisma.todo.findUnique({
-            where: {
-                userId: session?.user?.id,
-            },
-            select: {
-                id: true,
-            }
-        })
+  if (!input.title) {
+    return {
+      error: "Title is required",
+      data: null,
+    };
+  }
 
-        if(!todo) {
-            return {
-                error: 'Not Found',
-                data: null,
-            }
-        }
+  const todo = await prisma.todo.create({
+    data: {
+      title: input.title,
+      userId: session?.user?.id,
+    },
+  });
 
-        const updatedTodo = await prisma.todo.update({
-            where: {
-                id: input.id,
-                userId: session?.user?.id,
-            },
-            data: {
-                title: input.title,
-                doneAt: input.doneAt,
-            },
-          
-        })
-        return {
-            error: null,
-            data: updatedTodo,
-        }
-    }
-
-    if(!input.title) {
-        return {
-            error: "Title is required",
-            data: null
-        }
-    }
-        
-    const todo = await prisma.todo.create({
-   data: {
-    title: input.title,
-                userId: session?.user?.id,
-   },
-    })
-    
-        
-        return todo
-    }
+  return todo;
+}
